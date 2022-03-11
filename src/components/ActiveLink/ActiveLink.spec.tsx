425a4d51b339990e3bed8d5f5c8e96d21e54ef0b
AlanisMayumi <mayumi.alanis@gmail.com>
2022-03-10 23:34:30 -0300

    teste - describe

--- src/components/ActiveLink/ActiveLink.spec.tsx
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -11,12 +11,24 @@ jest.mock("next/router", () => {
   };
 });
 
-test("active link renders correctlly", () => {
-  const { getByText } = render(
-    <ActiveLink href="/" activeClassName="active">
-      <a>Home</a>
-    </ActiveLink>
-  );
+describe("ActiveLink component", () => {
+  test("active link renders correctlly", () => {
+    const { getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>
+    );
 
-  expect(getByText("Home")).toBeInTheDocument();
+    expect(getByText("Home")).toBeInTheDocument();
+  });
+
+  test("active link is receiving active class", () => {
+    const { getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>
+    );
+
+    expect(getByText("Home")).toHaveClass("active");
+  });
 });
